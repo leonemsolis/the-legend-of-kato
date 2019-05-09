cf932856c143e61236420c18236e492319c6e058
Ruslan <leonemsolis@gmail.com>
2019-05-09 22:29:06 +0900

    gitignore managing pt2

---
+++ The Legend of Kato/Assets/Scripts/ButtonActive.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActive : MonoBehaviour
+{
+    PlayerController player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    private void OnMouseDown()
+    {
+        player.Jump();
+    }
+}
---
+++ The Legend of Kato/Assets/Scripts/Health.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    int currentHealth = 3;
+
+    public void RestoreHealth()
+    {
+        transform.GetChild(currentHealth).GetComponent<SpriteRenderer>().color = Color.red;
+        currentHealth++;
+        if(currentHealth > 3)
+        {
+            currentHealth = 3;
+        }
+    }
+
+    public void TakeDamage()
+    {
+        currentHealth--;
+        transform.GetChild(currentHealth).GetComponent<SpriteRenderer>().color = Color.white;
+        if(currentHealth == 0)
+        {
+            Destroy(FindObjectOfType<PlayerController>().gameObject);
+        }
+    }
+}
