6a78c25de0a0f56483d59e8203a4e99526efae2e
Ruslan <leonemsolis@gmail.com>
2019-05-11 03:41:24 +0900

    reset commit

--- The Legend of Kato/Assets/Scripts/Health.cs
+++ The Legend of Kato/Assets/Scripts/Health.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class Health : MonoBehaviour
 {
     int currentHealth = 3;
-    bool invulnerable = false;
+    bool canTakeDamage = true;
     SpriteRenderer playerSpriteRenderer;
 
     private void Awake()
@@ -25,6 +25,23 @@ public class Health : MonoBehaviour
     }
 
     public void TakeDamage()
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    {
+        if(canTakeDamage)
+        {
+            currentHealth--;
+            transform.GetChild(currentHealth).GetComponent<SpriteRenderer>().color = Color.white;
+            canTakeDamage = false;
+            playerSpriteRenderer.color = Color.red;
+            StartCoroutine(ResetInvulnerable());
+            if (currentHealth == 0)
+            {
+                Destroy(FindObjectOfType<PlayerController>().gameObject);
+                SceneManager.LoadScene(0);
+            }
+=======
     {
         currentHealth--;
         transform.GetChild(currentHealth).GetComponent<SpriteRenderer>().color = Color.white;
@@ -35,13 +52,38 @@ public class Health : MonoBehaviour
         {
             Destroy(FindObjectOfType<PlayerController>().gameObject);
             SceneManager.LoadScene(0);
+>>>>>>> parent of 8a78d1b... Fix health, fix control on mobile devices
+=======
+    {
+        currentHealth--;
+        transform.GetChild(currentHealth).GetComponent<SpriteRenderer>().color = Color.white;
+        invulnerable = true;
+        playerSpriteRenderer.color = Color.red;
+        StartCoroutine(ResetInvulnerable());
+        if (currentHealth == 0)
+        {
+            Destroy(FindObjectOfType<PlayerController>().gameObject);
+            SceneManager.LoadScene(0);
+>>>>>>> parent of 8a78d1b... Fix health, fix control on mobile devices
+=======
+    {
+        currentHealth--;
+        transform.GetChild(currentHealth).GetComponent<SpriteRenderer>().color = Color.white;
+        invulnerable = true;
+        playerSpriteRenderer.color = Color.red;
+        StartCoroutine(ResetInvulnerable());
+        if (currentHealth == 0)
+        {
+            Destroy(FindObjectOfType<PlayerController>().gameObject);
+            SceneManager.LoadScene(0);
+>>>>>>> parent of 8a78d1b... Fix health, fix control on mobile devices
         }
     }
 
     private IEnumerator ResetInvulnerable()
     {
         yield return new WaitForSeconds(1f);
-        invulnerable = true;
+        canTakeDamage = true;
         playerSpriteRenderer.color = Color.white;
     }
 }
