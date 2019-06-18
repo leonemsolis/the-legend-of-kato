cb56f2c411fc77c012732a20dc71da7c04b71b69
Ruslan <leonemsolis@gmail.com>
2019-06-19 03:37:17 +0900

    Update Unity version

---
+++ The Legend of Kato/Assets/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    // Each inner list contains rooms that on the "same" y-axis
+    [SerializeField] List<List<GameObject>> rooms;
+    // Each element is a hall that separates pair of rooms
+    [SerializeField] List<GameObject> halls;
+
+    void Update()
+    {
+        // When Character enters certain room, remove all rooms & halls that are on the left
+        // + Also block doors on the same y-axis rooms
+        //
+        // When Character leaves certain room??
+    }
+}
--- The Legend of Kato/Assets/Scripts/Game/C.cs
+++ The Legend of Kato/Assets/Scripts/Game/C.cs
@@ -4,12 +4,14 @@
     public const float HalfUnit = 50f;
     public const float ButtonPanelHeight = 400f;
     public const float InfoPanelHeight = 200f;
+    public const int RoomIDBoss = 1000;
 
     // Tags
     public const string PlayerTag = "Player";
     public const string BlockTag = "Block";
     public const string RoomTag = "Room";
     public const string InvulnirableEnemyHitBoxTag = "InvulnirableEnemyHitBox";
-
+    public const string BootsTag = "Boots";
+    public const string SwordTag = "Sword";
 
 }
